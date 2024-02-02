d277352dbf3f08e942164677d4388b58350ed5b8
ghostNoté <selcukbilgen@hotmail.com>
2024-02-02 14:47:58 +0300

    get data

--- Web.Api/Program.cs
+++ Web.Api/Program.cs
@@ -1,13 +1,14 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Web.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +17,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("customers", async (IConfiguration configuration) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    var connectionString = configuration.GetConnectionString("DefaultConnection")!;
+    using var connection = new SqlConnection(connectionString);
+    const string sql = "SELECT Id, FirstName, LastName, Email, DateOfBirth FROM Customers";
+    var customers = await connection.QueryAsync<Customer>(sql);
+    return Results.Ok(customers);
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
